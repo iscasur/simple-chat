18960f776844d580accb6b09f3beb5c981b200ed
Israel Castro <israel.castro@gmail.com>
2021-06-23 18:58:10 -0500

    [Done] client-server connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import socket from './Components/Socket';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  socket.emit('connected', 'Hello from client!');
+
+  return <div className='App'></div>;
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -3,16 +3,15 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const socket = require('socket.io');
 const io = socket(server);
 
 io.on('connection', (socket) => {
-
   socket.on('connected', () => {
-    console.log('User connected')
-  })
-})
+    console.log('User connected');
+  });
+});
 
 server.listen(PORT, () => console.log('Server started'));
