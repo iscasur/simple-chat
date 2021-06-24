055679d4280e472e346b587d4d4fd97861ac42be
Israel Castro <israel.castro@gmail.com>
2021-06-23 19:13:23 -0500

    [Add] orders received from client

--- server/index.js
+++ server/index.js
@@ -12,6 +12,14 @@ io.on('connection', (socket) => {
   socket.on('connected', () => {
     console.log('User connected');
   });
+
+  socket.on('message', (name, message) => {
+    io.emit('messages', { name, message });
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('messages', { server: 'Server', message: 'Has left the room' });
+  });
 });
 
 server.listen(PORT, () => console.log('Server started'));
