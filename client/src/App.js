06dc6343023698a2ecaee2b20b87cad0652d03f6
Israel Castro <israel.castro@gmail.com>
2021-06-23 19:02:12 -0500

    [Add] register form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
+import React, { useState } from 'react';
 import './App.css';
 import socket from './Components/Socket';
 
 function App() {
-  socket.emit('connected', 'Hello from client!');
+  const [name, setName] = useState('');
+  const [user, setUser] = useState(false);
 
-  return <div className='App'></div>;
+  const register = (e) => {
+    e.prevenDefault();
+    if (name !== '') {
+      setUser(true);
+    }
+  };
+
+  return (
+    <div className='App'>
+      <form onSubmit={register}>
+        <label htmlFor=''>Name:</label>
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button>Enter</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
